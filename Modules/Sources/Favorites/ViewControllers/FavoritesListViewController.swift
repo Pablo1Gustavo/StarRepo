45f3371caa345eecddd714eb242d94c5f478aca4
Felipe Vidal <felipeisra.vidal@gmail.com>
2022-03-30 15:42:03 -0300

    Add FavoritesList preview

--- Modules/Sources/Favorites/ViewControllers/FavoritesListViewController.swift
+++ Modules/Sources/Favorites/ViewControllers/FavoritesListViewController.swift
@@ -21,10 +21,52 @@ public class FavoritesListViewController: UIViewController {
 
     public override func viewDidLoad() {
         super.viewDidLoad()
+        
+        configureNavigationBar()
     }
     
     // MARK: - Private methods
     
+    private func configureNavigationBar() {
+        title = "Favorites"
+        
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
     // MARK: - Public methods
 
 }
+
+#if DEBUG
+import SwiftUI
+
+struct FavoritesListViewControllerPreviews: PreviewProvider {
+    static var previews: some View {
+        if #available(iOS 14.0, *) {
+            ContainerPreview()
+                .ignoresSafeArea()
+        } else {
+            ContainerPreview()
+                .environment(\.colorScheme, .dark)
+        }
+    }
+    
+    struct ContainerPreview: UIViewControllerRepresentable {
+        typealias UIViewControllerType = UINavigationController
+        
+        func makeUIViewController(context: Context) -> UIViewControllerType {
+            let viewModel = FavoritesListViewModel()
+            
+            let viewController = FavoritesListViewController(
+                viewModel: viewModel
+            )
+            
+            let navController = UINavigationController(rootViewController: viewController)
+            
+            return navController
+        }
+        
+        func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {}
+    }
+}
+#endif
